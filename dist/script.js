b5ca2e9d659ad6437a9a8377cf3a8a637fb2f301
gkchahal <59899312+gkchahal@users.noreply.github.com>
2020-01-20 17:29:38 -0500

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,3 @@
+var name = prompt("What is your first name?")
+var color = prompt("What is your favorite color?")
+document.body.innerHTML = "My first name is"name+" and my favorite color is"+color;
